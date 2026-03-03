597a5fee1e42839c5aa82f68312e327cb8c1ffba
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-03-03 16:41:25 -0300

    feat: adiciona cors, morgan e helmet

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const carsRoutes = require("./routes/cars.routes");
-
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.use("/cars", carsRoutes);
 

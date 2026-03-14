b8e0ce77eb9ff9b40cae97ffecc8020c727d8e0d
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-03-13 23:00:34 -0300

    feat: adiciona funcionalidade de gerar dados

---
+++ cypress/support/factories/car.factory.js
@@ -0,0 +1,14 @@
+const { faker } = require("@faker-js/faker");
+
+export const CarFactory = {
+  createCar(overrides = {}) {
+    return {
+      nome: faker.vehicle.vehicle(),
+      marca: faker.vehicle.manufacturer(),
+      modelo: faker.vehicle.model(),
+      ano: faker.number.int({ min: 1990, max: 2025 }),
+      preco: faker.number.int({ min: 20000, max: 300000 }),
+      ...overrides,
+    };
+  },
+};

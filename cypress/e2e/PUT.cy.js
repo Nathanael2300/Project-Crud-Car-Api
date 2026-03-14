e415c01b3dbf2bfa10560f5a56637f7a549b475a
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-03-13 22:43:25 -0300

    refactor: substituir criação manual de objeto por CarFactory em PUT.cy.js

--- cypress/e2e/PUT.cy.js
+++ cypress/e2e/PUT.cy.js
@@ -1,9 +1,10 @@
 import "cypress-mochawesome-reporter/register";
-const CarsApi = require("../support/api/carsApi");
+const CarsApi = require("../support/api/cars.service");
 const { faker } = require("@faker-js/faker");
+import { CarFactory } from "../support/factories/car.factory";
 
 describe("PUT /cars", () => {
-  const Car = {
+  const CarUpdate = {
     nome: faker.vehicle.vehicle(),
     marca: faker.vehicle.manufacturer(),
     modelo: faker.vehicle.model(),
@@ -13,39 +14,47 @@ describe("PUT /cars", () => {
 
   it("Should update some field of the car", () => {
     const api = new CarsApi();
+    const car = CarFactory.createCar();
 
-    const dataUpdate = { ...Car };
-    return api.updateCar(1, dataUpdate).then((res) => {
-      expect(res.status).to.be.eql(200);
-      expect(res.body).to.have.property(
+    return api.createCar(car).then((createRes) => {
+      expect(createRes.body).to.have.property(
         "message",
-        "Carro atualizado com sucesso",
+        "Carro criado com sucesso",
       );
+      expect(createRes.status).to.eql(201);
+      expect(createRes.body.car).to.have.property("id");
+      const id = createRes.body.car.id;
+      return api.updateCar(id, CarUpdate).then((UpdateRes) => {
+        expect(UpdateRes.status).to.be.eql(200);
+        expect(UpdateRes.body).to.have.property(
+          "message",
+          "Carro atualizado com sucesso",
+        );
+        const expectedStructure = {
+          id: "number",
+          nome: "string",
+          modelo: "string",
+          marca: "string",
+          ano: "number",
+          preco: "number",
+        };
 
-      const expectedStructure = {
-        id: "number",
-        nome: "string",
-        modelo: "string",
-        marca: "string",
-        ano: "number",
-        preco: "number",
-      };
+        for (const [field, type] of Object.entries(expectedStructure)) {
+          expect(UpdateRes.body.car).to.have.property(field);
+          expect(UpdateRes.body.car[field]).to.be.a(type);
+        }
 
-      for (const [field, type] of Object.entries(expectedStructure)) {
-        expect(res.body.car).to.have.property(field);
-        expect(res.body.car[field]).to.be.a(type);
-      }
-
-      const id = res.body.car.id;
-      const expectedValue = {
-        id: id,
-        nome: Car.nome,
-        modelo: Car.modelo,
-        marca: Car.marca,
-        ano: Car.ano,
-        preco: Car.preco,
-      };
-      expect(res.body.car).to.deep.include(expectedValue);
+        const id = createRes.body.car.id;
+        const expectedValue = {
+          id: id,
+          nome: CarUpdate.nome,
+          modelo: CarUpdate.modelo,
+          marca: CarUpdate.marca,
+          ano: CarUpdate.ano,
+          preco: CarUpdate.preco,
+        };
+        expect(UpdateRes.body.car).to.deep.include(expectedValue);
+      });
     });
   });
 
@@ -59,7 +68,7 @@ describe("PUT /cars", () => {
   for (const [field, invalidValue] of Object.entries(invalidFields)) {
     it(`Should not update car when ${field} was invalid type`, () => {
       const api = new CarsApi();
-      return api.createCar(Car).then((createRes) => {
+      return api.createCar(CarUpdate).then((createRes) => {
         expect(createRes.status).to.eql(201);
 
         const id = createRes.body.id;
